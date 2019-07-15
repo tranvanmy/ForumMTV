cd9b8decd836f9c87c32a9e369f4ff1ac7638585
vanmy <anhmynd19101995@gmail.com>
2019-07-15 23:26:46 +0700

    add_snow

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -20,5 +20,22 @@
 
         <script src="{{ asset('frontend/js/app.js') }}"></script>
         <script src="{{ asset('frontend/dist/wedding/js/wedding.min.js') }}"></script>
+        <script>
+            $('body').flurry({
+              character: "~",
+              color: ["#55476A", "#AE3D63", "#DB3853", "#F45C44", "#F8B646"],
+              speed: 2000,
+              height: 680,
+              frequency: 60,
+              small: 12,
+              large: 50,
+              rotation: 90,
+              rotationVariance: 20,
+              startRotation: 90,
+              wind: 25,
+              windVariance: 100,
+              opacityEasing: "cubic-bezier(1,0,.96,.9)"
+            });
+        </script>
     </body>
 </html>
